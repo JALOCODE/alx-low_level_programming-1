a90c3afec01238f3c41c6ca466875b2edcd1026d
Hart-Ijere <ijerehart@gmail.com>
2022-09-13 21:37:43 -0700

    Modified main.c file

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  char *sh = "main.h";
+
+  while (*sh)
+    {
+      _putchar(*sh);
+      sh++;
+    }
+  _putchar('\n');
+  return 0;
+}

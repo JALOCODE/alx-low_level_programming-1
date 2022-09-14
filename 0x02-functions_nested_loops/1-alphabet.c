f693a3332764d3e6253a7498e8cf5e19ec786a1d
Hart-Ijere <ijerehart@gmail.com>
2022-09-14 10:38:52 -0700

    Print alphabet in lower letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - Entry poin
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+  char c = 'a';
+
+  while (c <= 'z')
+    {
+      _putchar(c);
+      c++;
+    }
+  _putchar('\n');
+
+
+}

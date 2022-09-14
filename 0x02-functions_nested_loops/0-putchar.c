055ce1f97a157b4f2048c8f350bd26f6071d79f2
Hart-Ijere <ijerehart@gmail.com>
2022-09-14 10:20:30 -0700

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <main.h>
-/** Write a program that prints _putchar.
- *
- * The program should return 0.
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * Return: Always 0 (success)
  */
-int main ()
+
+int main(void)
 {
-  char _putchar = G;
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
 
-  putchar(_putchar\n);
-
-  return 0;
+  return (0);
 }
-  
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,20 @@
-#include "main.h"
-/**
- * main - prints _putchar
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
+#ifndef MAIN_H
+#define MAIN_H
 
-  return (0);
+void _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int main(void);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int ab);
+int print_last_digit(int nld);
+void jack_bauer(void);
+void time_table(void);
+int add(int one, int two);
+void print_to_98(int n);
+void print_times_table(int n);
+int main(void);
 
-
-}
+#endif /* MAIN_H */
